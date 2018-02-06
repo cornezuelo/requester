a368086425ab7d0ca880630d60cadd781b858664
Oscar Aviles Miramontes <emeeseka01@gmail.com>
2018-02-06 16:22:58 +0100

    Placeholder buttons, changing layout

--- index.php
+++ index.php
@@ -36,6 +36,11 @@
                     <input type="text" class="form-control" id="request-uri" name="request-uri" aria-describedby="request-uri" placeholder="http://www.uri.com...">                  
                   </div>                
                 </div>
+                <div class="form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Follow redirections</label>
+                </div>          
+                <hr>
                 <div class="form-group">
                   AUTHENTICATION
                 </div>
@@ -44,14 +49,18 @@
                 </div>
                 <div class="form-group">
                   PARÁMETROS
+                </div>                
+                <hr>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <button type="button" class="btn btn-outline-primary btn-sm" title="Save"><i class="fas fa-save"></i> Save</button>          
+                        <button type="button" class="btn btn-outline-primary btn-sm" title="Load"><i class="fas fa-folder-open"></i> Load</button>          
+                    </div>
+                    <div class="col-sm-8" align="right">                    
+                        <button type="submit" class="btn btn-primary btn-lg" title="Send"><i class="fas fa-share-square"></i> Send</button>
+                    </div>
                 </div>
-                <div class="form-check">
-                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                  <label class="form-check-label" for="exampleCheck1">Follow redirections</label>
-                </div>
-                <div align="right">
-                  <button type="submit" class="btn btn-primary">Send</button>
-                </div>
+                <div>                                        
               </form>                                         
         </div>
       </div>
@@ -69,6 +78,7 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
