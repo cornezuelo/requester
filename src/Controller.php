bf822456c2cfcbbb1826b1a718b9ddf2a8aaee0d
Oscar Aviles Miramontes <emeeseka01@gmail.com>
2018-02-08 11:36:17 +0100

    Fix data

--- src/Controller.php
+++ src/Controller.php
@@ -4,33 +4,34 @@ if (!isset($_REQUEST['action'])) {
     die('No action sent.');
 }
 switch ($_REQUEST['action']) {
-    case 'request':
-        if (isset($_REQUEST['request-uri'])) {        
+    case 'request':        
+        parse_str($_REQUEST['data'],$data);        
+        if (isset($data['request-uri'])) {        
             $options = ['json' => 1];
             $params = [];
-            if (isset($_REQUEST['request-followredirections'])) {
+            if (isset($data['request-followredirections'])) {
                 $options['setopt'][CURLOPT_FOLLOWLOCATION] = 1;
             }    
-            if (isset($_REQUEST['request-header-keys']) && !empty($_REQUEST['request-header-keys'])) {
-               foreach ($_REQUEST['request-header-keys'] as $k => $v) {
+            if (isset($data['request-header-keys']) && !empty($data['request-header-keys'])) {
+               foreach ($data['request-header-keys'] as $k => $v) {
                    if (!empty($v) && isset($_REQUEST['request-header-values'][$k])) {
                        $options['headers'][$v] = $_REQUEST['request-header-values'][$k];
                    }
                } 
             }
-            if (isset($_REQUEST['request-params-keys']) && !empty($_REQUEST['request-params-keys'])) {
-                foreach ($_REQUEST['request-params-keys'] as $k => $v) {
-                   if (!empty($v) && isset($_REQUEST['request-params-values'][$k])) {
-                       $params[$v] = $_REQUEST['request-params-values'][$k];
+            if (isset($data['request-params-keys']) && !empty($data['request-params-keys'])) {
+                foreach ($data['request-params-keys'] as $k => $v) {
+                   if (!empty($v) && isset($data['request-params-values'][$k])) {
+                       $params[$v] = $data['request-params-values'][$k];
                    }
                }         
             }
 
-            if ($_REQUEST['type-params'] == 'httpquery' && !empty($_REQUEST['textarea-params-httpquery-content'])) {
-                $options['httpquery'] = $_REQUEST['textarea-params-httpquery-content'];
+            if ($data['type-params'] == 'httpquery' && !empty($data['textarea-params-httpquery-content'])) {
+                $options['httpquery'] = $data['textarea-params-httpquery-content'];
             }
 
-            $res = Requester::request($_REQUEST['request-method'],$_REQUEST['request-uri'],$params,$options);      
+            $res = Requester::request($data['request-method'],$data['request-uri'],$params,$options);                  
             print_r($res);die();    
         }    
         break;
